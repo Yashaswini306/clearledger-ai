7c06c785ca8726c1f1e354a131d5612bc576d81c
Yashaswini.I.R <yashaswiniiryashaswiniir844@gmail.com>
2026-05-25 10:36:45 +0000

    ClearLedger AI

---
+++ client/public/server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB Connected'))
+
+app.get('/', (req, res) => {
+  res.send('Backend Running')
+})
+
+app.listen(5000, () => {
+  console.log('Server running on 5000')
+})
---
+++ client/public/src/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>ClearLedger AI Dashboard</h1>
+    </div>
+  )
+}
